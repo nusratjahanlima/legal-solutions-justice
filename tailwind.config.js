60dbab5397f033b9c618504bf3761ab44bf48754
nusratjahanlima <nusratjahanlima639@gmail.com>
2024-01-22 18:10:50 +0600

    navbar,banner and footer section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'programming': "url(./images/banner.png)",
+      }
+
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
